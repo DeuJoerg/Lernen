769882a16432b563ee38acdbb8ac76602da3d2d2
DeuJoerg <joergochs@gmx.net>
2023-03-25 18:39:33 +0100

    test

---
+++ Pycharmtutorial.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def say_hello():
+    print("hallo")
+
---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from Pycharmtutorial import say_hello
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+def print_hi_three_times():
+    print("hi")
+    print("hi")
+    print("hi")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("hi")
+    print_hi_three_times()
+    # print_hi('PyCharm')
+    # print("hi")
+    # say_hello()
+# See PyCharm help at https://www.jetbrains.com/hepython.exe -m pip install --upgrade piplp/pycharm/
